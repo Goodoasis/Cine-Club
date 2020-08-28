dba40f2d0e9dcfa72468521c7134124f829e54f8
GoodOasis <de.thibault@gmail.com>
2020-08-29 00:12:23 +0200

    make getter & setter for class Movie

--- movie.py
+++ movie.py
@@ -1,3 +1,8 @@
+import os
+import json
+
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE =  os.path.join(CUR_DIR, "data", "movies.json")
 
 
 class Movie():
@@ -18,6 +23,13 @@ class Movie():
         """
         return self.title
 
+    def _get_movies(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+
+    def _write_movies(self, movies):
+        with open(DATA_FILE, "w") as f:
+            json.dump(movies, f, indent=4)
 
 if __name__ == '__main__':
     b= Movie("interstellar")

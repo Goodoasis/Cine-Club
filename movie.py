31f38fda66fc8dc13d5b336e8e36b4c715f7906b
GoodOasis <de.thibault@gmail.com>
2020-08-29 08:38:43 +0200

    Add public methode to add movie on if he doesnt exists yet

--- movie.py
+++ movie.py
@@ -1,5 +1,6 @@
 import os
 import json
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE =  os.path.join(CUR_DIR, "data", "movies.json")
@@ -30,7 +31,23 @@ class Movie():
     def _write_movies(self, movies):
         with open(DATA_FILE, "w") as f:
             json.dump(movies, f, indent=4)
+        
+    def add_to_movies(self):
+        # recuperer la liste
+        movies = self._get_movies()
+
+        # verifier si le film est deja dans la liste
+        if self.title not in movies:
+            movies.append(self.title)
+            self._write_movies(movies)
+            return True
+        # sînon on log un message pour dire a luser
+        # que le films'est deja dans la liste
+        else:
+            logging.warning(f"  Le film: '{self.title}' est déjà dans la liste.")
+            return False
+
 
 if __name__ == '__main__':
     b= Movie("interstellar")
-    print(dir(b))
+    print(b.add_to_movies())

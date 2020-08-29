24f9a418ac5bceb0e799a2c342c7325cb1cfdfd6
GoodOasis <de.thibault@gmail.com>
2020-08-29 22:20:57 +0200

    All methodes are done. Need yet com's and docstrings.

--- movie.py
+++ movie.py
@@ -5,9 +5,18 @@ import logging
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE =  os.path.join(CUR_DIR, "data", "movies.json")
 
+def get_movies():
+        with open(DATA_FILE, "r") as f:
+            movies_title = json.load(f)
+        movies = [Movie(movie_title) for movie_title in movies_title]
+        return movies
+
 
 class Movie():
-    """  """
+
+    """ 
+    """
+
     def __init__(self, title):
         """[Class constructor]
 
@@ -25,29 +34,49 @@ class Movie():
         return self.title
 
     def _get_movies(self):
+        """ """
         with open(DATA_FILE, "r") as f:
             return json.load(f)
 
     def _write_movies(self, movies):
+        """ """
         with open(DATA_FILE, "w") as f:
             json.dump(movies, f, indent=4)
-        
-    def add_to_movies(self):
-        # recuperer la liste
-        movies = self._get_movies()
 
-        # verifier si le film est deja dans la liste
+    def add_to_movies(self):
+        """ """
+        movies = self._get_movies()
+        
         if self.title not in movies:
             movies.append(self.title)
             self._write_movies(movies)
             return True
-        # sînon on log un message pour dire a luser
-        # que le films'est deja dans la liste
         else:
-            logging.warning(f"  Le film: '{self.title}' est déjà dans la liste.")
+            logging.warning(f"  Le film: '{self.title}' est enregistré.")
             return False
 
+    def remove_to_movies(self):
+        """ """
+        movies = self._get_movies()
+
+        if self.title in movies:
+            movies.remove(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f"  Suppression impossible:  '{self.title}' n'est pas enregistré.")
+            return False            
 
 if __name__ == '__main__':
     b= Movie("interstellar")
     print(b.add_to_movies())
+    m = Movie("Harry potter")
+    c = Movie("matrix")
+    print(c.add_to_movies())
+    print(m.add_to_movies())
+    u = Movie("Arthur et les minimoys")
+    print(u.add_to_movies())
+    T = Movie("Avengers")
+    T.remove_to_movies()
+    listy = get_movies()
+    print(listy)

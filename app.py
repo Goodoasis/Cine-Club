2c84d02b0207b9ab3cea79e20d0cbe9e8ae34260
GoodOasis <de.thibault@gmail.com>
2020-08-30 17:05:05 +0200

    add some commentary and docstrings

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from PySide2 import QtWidgets, QtCore
+
 from movie import Movie, get_movies
+
 
 class App(QtWidgets.QWidget):
     """
+    Interface graphique d'une appmication qui permet de lister des
+    titres de film, de sauvegarder cette listeet enfin de les
+    supprimer.
+
     """
 
     def __init__(self):
-        """ """
+        """Constructeur de notre interface graphique."""
         super().__init__()
         self.setWindowTitle("Ciné Club")
-        # Ajout des widgets
-        self.setup_ui()
+        self.setup_ui()  # Ajout des Widgets.
         self.populate_movies()
-        # Création des connexion entre widgets
-        self.setup_connexions()
+        self.setup_connexions()  # Création des connexion entre widgets.
 
     def setup_ui(self):
-        """ """
+        """Creation et dispositions des widgets."""
         # Creation du layout
         self.layout = QtWidgets.QVBoxLayout(self)
         # Creation des widgets
@@ -32,45 +36,50 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.btn_removeMovies)
 
     def populate_movies(self):
-        """ """
+        """
+        Remplis le ListWidget des films contenus dans data\movies.json.
+        """
         self.lw_movies.clear()
         for movie in get_movies():
             lw_item = QtWidgets.QListWidgetItem(movie.title)
-            # Methode pour joindre un objet a un str de ListWidget
+            # Methode pour joindre un objet a un str de ListWidget.
             lw_item.setData(QtCore.Qt.UserRole, movie)
-            # Ajout du contenu du json dans ListWidget
+            # Ajout du contenu du json dans ListWidget.
             self.lw_movies.addItem(lw_item)
 
     def setup_connexions(self):
-        """ """
+        """Creation des connexions"""
         self.btn_addMovie.clicked.connect(self.add_movie)
+        # Reaction du bouton entrée si focus sur le lineEdit.
         self.le_movieTitle.returnPressed.connect(self.add_movie)
-
         self.btn_removeMovies.clicked.connect(self.remove_movie)
 
     def add_movie(self):
-        """ """
-        # Récupérer le text du lineEdit et Creer une instance Movie
+        """Méthode pour ajouter un film a partir du LineEdit.
+        Vérifie si ce film n'existe pas déjà avant de l'enregistrer"""
+        # Récupérer le text du lineEdit si non vide.
         movie_title = self.le_movieTitle.text()
         if not movie_title:
             return False
-        movie= Movie(movie_title)
-        result = movie.add_to_movies()
-        # Ajouter le film dans le Json
+
+        movie= Movie(movie_title)  #  Creation de l'instance Movie.
+        result = movie.add_to_movies()  # Ajout du film dans le Json.
+
         if result:
             lw_item = QtWidgets.QListWidgetItem(movie.title)
             lw_item.setData(QtCore.Qt.UserRole, movie)
-            # Ajouter le film dans le ListWidget
+            # Ajouter le film dans le ListWidget.
             self.lw_movies.addItem(lw_item)
         
         self.le_movieTitle.setText("")
 
     def remove_movie(self):
-        """ """
+        """Méthode pour supprimer le(s) film(s) selectionnés."""
         for selected_item in self.lw_movies.selectedItems():
             movie = selected_item.data(QtCore.Qt.UserRole)
             movie.remove_from_movies()
             self.lw_movies.takeItem(self.lw_movies.row(selected_item))
+
 
 app = QtWidgets.QApplication([])
 win = App()
--- movie.py
+++ movie.py
@@ -2,10 +2,12 @@ import os
 import json
 import logging
 
+# Creation du chemin d'acces au Json.
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE =  os.path.join(CUR_DIR, "data", "movies.json")
 
 def get_movies():
+    """ Fonction récuperant la liste des films de Json. """
         with open(DATA_FILE, "r") as f:
             movies_title = json.load(f)
         movies = [Movie(movie_title) for movie_title in movies_title]
@@ -13,38 +15,38 @@ def get_movies():
 
 
 class Movie():
-
-    """ 
+    """
+    Classe movie qui formatte un titre d'un film.
+    avec des méthodes permettant d'enregistrer le titre
+    dans un fichier json.
     """
 
     def __init__(self, title):
-        """[Class constructor]
-
-        Args:
-            title ([string]): [movie title]
+        """
+        Constructeur de classe ne prenant q'un
+        argument de type str puis le formate.
         """
         self.title = title.title()
     
     def __str__(self):
-        """[objet to string]
-
-        Returns:
-            [str]: [return a printable of Movie object]
-        """
+        """Méthode retournant une version str de l'obj."""
         return self.title
 
     def _get_movies(self):
-        """ """
+        """Méthode privé; retourne le contenue du Json."""
         with open(DATA_FILE, "r") as f:
             return json.load(f)
 
     def _write_movies(self, movies):
-        """ """
+        """Méthode privé; enregistre arg dans le Json."""
         with open(DATA_FILE, "w") as f:
             json.dump(movies, f, indent=4)
 
     def add_to_movies(self):
-        """ """
+        """
+        Méthode qui enregistre le film dans le json en
+        verifiant au préalable de ne pas faire de doubon.
+        """
         movies = self._get_movies()
         
         if self.title not in movies:
@@ -56,7 +58,7 @@ class Movie():
             return False
 
     def remove_from_movies(self):
-        """ """
+        """Méthode qui supprime le film si il existe."""
         movies = self._get_movies()
 
         if self.title in movies:
